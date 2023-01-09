5dd96ce499569c143070c0f3f78c6858fcc2e192
Bexa <bexins@gmail.com>
2023-01-09 12:57:58 +0300

    feat: add complete solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {  
+  const str = n.toString();
+  let arr = [];
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    if (Number(str[i]) || Number(str[i]) === 0) {arr.push(str[i]);console.log(str[i])} 
+  }
+  return Number(arr.join(''));
 }
